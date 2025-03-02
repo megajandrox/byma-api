69e86f19952fcc840fce57a2c64c95d7d3f03323
Alejandrol <megajandrox@gmail.com>
2025-03-01 23:20:04 -0300

    fixing docker

--- parameters.py
+++ parameters.py
@@ -2,8 +2,7 @@ import os
 
 # 1. Get the environment variable (with a default value):
 #DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///investing.db")
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://admin:admin123@localhost:5432/postgres")
-
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://admin:admin123@postgres_db_byma_api:5432/postgres")
 # Explanation:
 # os.environ.get("DATABASE_URL", "sqlite:///investing.db") tries to retrieve the value of the
 # environment variable named "DATABASE_URL". If the environment variable is not set, it
